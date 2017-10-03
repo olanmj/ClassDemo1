6ced2f6c42e84effacee631fc7444e386d3916d8
olanmj <olanm@stockton.edu>
2017-10-03 09:52:21 -0400

    Minimal app with static html page

--- ClassDemo1/Startup.cs
+++ ClassDemo1/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,9 +21,12 @@ namespace ClassDemo1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello MVC World!");
             });
         }
     }
